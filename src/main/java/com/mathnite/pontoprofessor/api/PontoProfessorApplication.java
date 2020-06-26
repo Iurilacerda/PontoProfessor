97b1b47d6e01b87fdfa9e89c3b7893cb365971d6
Iuri Lacerda <iuri.lacerda.souza@gmail.com>
2020-06-26 15:33:03 -0300

    API para o cartão ponto dos professores.

---
+++ src/main/java/com/mathnite/pontoprofessor/api/PontoProfessorApplication.java
@@ -0,0 +1,13 @@
+package com.mathnite.pontoprofessor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoProfessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathnite/pontoprofessor/api/PontoProfessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathnite.pontoprofessor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoProfessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
